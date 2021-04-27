1d0bd40642adc59b9c42f560414ad5f97838138d
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2021-04-27 14:16:22 -0400

    Added hero image and styled navbar

--- client/src/component/NavBar/NavBar.jsx
+++ client/src/component/NavBar/NavBar.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import { Navbar, NavDropdown, Nav, FormControl, Form, Button } from 'react-bootstrap';
-
+import './NavBar.css'
 
 const NavBar = () => {
     return (
-        <div>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <div className="text-center">
+            <Navbar variant="light" expand="lg" id="navBar" className="navbar-custom text-center">
+                {/* <Navbar.Brand href="#home">HWK</Navbar.Brand> */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    <Nav className="mx-auto py-3">
+                        <Nav.Link href="#home" className="mx-2 px-4 navLink">Home</Nav.Link>
+                        <Nav.Link href="#link" className="mx-2 px-4 navLink">About</Nav.Link>
+                        <Nav.Link href="#link" className="mx-2 px-4 navLink">Podcast</Nav.Link>
+                        <Nav.Link href="#link" className="mx-2 px-4 navLink">Contact</Nav.Link>
+                        <Nav.Link href="#link" className="mx-2 px-4 navLink">Products</Nav.Link>
+                        <NavDropdown title="Services" id="basic-nav-dropdown" className="mx-2 px-4 navLink">
+                            <NavDropdown.Item className="dropdownItem" href="#action/3.1">Mind</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdownItem" href="#action/3.2">Body</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdownItem" href="#action/3.3">Spirit</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdownItem" href="#action/3.4">Motherhood</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    <Form inline>
+                    {/* <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
+                        <Button variant="outline-dark">Search</Button>
+                    </Form> */}
                 </Navbar.Collapse>
             </Navbar>
         </div>
