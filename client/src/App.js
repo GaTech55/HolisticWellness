1096345a6fcc376e49223d4d9d435c8a9d51b4d1
Hayes11 <bhayes81@gmail.com>
2021-04-26 22:47:09 -0400

    added navbar component and container, and also added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Home from './containers/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './component/NavBar/NavBar';
 
 function App() {
   return (
     <div>
-     <Home />
+      <NavBar />
+      <Home />
     </div>
   );
 }
